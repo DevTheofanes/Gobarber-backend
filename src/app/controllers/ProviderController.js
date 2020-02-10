d878940042d83a3c337d3911b990b1501f5ffc6e
DevTheofanes <devtheofanes@gmail.com>
2020-02-10 11:06:40 -0300

    first commit

---
+++ src/app/controllers/ProviderController.js
@@ -0,0 +1,21 @@
+import User from "../models/User";
+import File from "../models/File";
+
+class ProviderController {
+  async index(req, res) {
+    const providers = await User.findAll({
+      where: { provider: true },
+      attributes: ["id", "name", "email", "avatar_id"],
+      include: [
+        {
+          as: "avatar",
+          model: File,
+          attributes: ["name", "path", "url"]
+        }
+      ]
+    });
+    return res.json(providers);
+  }
+}
+
+export default new ProviderController();
